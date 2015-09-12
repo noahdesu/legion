1c8304c088d2659e4fe5a9ae7fb917494726dd82
Noah Watkins <noahwatkins@gmail.com>
2015-09-12 11:26:36 -0600

    rados: add timing parser
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ test/rados_attach_subregion_parallel/plot.py
@@ -0,0 +1,36 @@
+import sys
+import re
+
+record_re = re.compile("domain point: (?P<pt>.+);(?P<what>.+)seconds: "\
+        "(?P<sec>\d+) nanos: (?P<nanosec>\d+)")
+
+write_begins = {}
+write_ends = {}
+read_ends = {}
+
+for line in sys.stdin.readlines():
+    m = record_re.match(line)
+    if not m:
+        continue
+
+    point = m.group("pt")
+    nsec = long(m.group("sec")) * 10**9 + \
+        long(m.group("nanosec"))
+
+    what = m.group("what")
+    if "write begins" in what:
+        write_begins[point] = nsec
+    elif "write ends" in what:
+        write_ends[point] = nsec
+    elif "read ends" in what:
+        read_ends[point] = nsec
+    else:
+        assert False
+
+for point, nsec in write_begins.items():
+    write_start = nsec
+    write_end = write_ends[point]
+    read_start = write_end
+    read_end = read_ends[point]
+    print point, write_start, write_end, write_end-write_start,\
+            read_start, read_end, read_end-read_start
